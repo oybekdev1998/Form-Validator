d6ac46307825b3952c948c1265bd305525fadd40
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2021-12-27 14:11:15 +0500

    Created check function and end form validator

--- script.js
+++ script.js
@@ -11,26 +11,61 @@ function showError(input, message) {
   formControl.className = 'form-control error';
   const small = formControl.querySelector('small');
   small.innerText = message;
-}
+};
 // Success function
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
-}
+};
 
 //Check required fields
 
 function checkRequired(inputArry) {
-  inputArry.forEach(function (input) {
-    console.log(input);
+  inputArry.forEach(function(input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    }
+    else{
+      showSuccess(input);
+    }
   });
 };
 
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+};
+
+// Check length
+function checkLength(input, min, max) {
+  if(input.value.length < min){
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  }
+  else if(input.value.length > max){
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+  }
+  else{
+    showSuccess(input);
+  }
+};
+
+// Check password match
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match')
+  }
+}
+
 // Valid email
 
-function isValidEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  return re.test(String(email).toLowerCase()); 
+function checkEmail(input) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  }
+  else{
+    showError(input, 'Email is not valid')
+  }
   
 };
 
@@ -39,4 +74,8 @@ function isValidEmail(email) {
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   checkRequired([username, email, password, password2]);
-})
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordMatch(password, password2);
+});
